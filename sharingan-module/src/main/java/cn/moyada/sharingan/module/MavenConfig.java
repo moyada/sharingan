c6c4092a2c14483d0a73246d5bccfc5de6030999
moyada <xykmoyada@gmail.com>
2018-09-24 21:50:06 +0800

    add nexus3 rest api

---
+++ sharingan-module/src/main/java/cn/moyada/sharingan/module/MavenConfig.java
@@ -0,0 +1,48 @@
+package cn.moyada.sharingan.module;
+
+import cn.moyada.sharingan.common.utils.StringUtil;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xueyikang
+ * @since 1.0
+ **/
+@Component
+@ConfigurationProperties(prefix = "maven")
+public class MavenConfig {
+
+    private String host;
+
+    private String username;
+
+    private String password;
+
+    public boolean isCredential() {
+       return !StringUtil.isEmpty(username) && !StringUtil.isEmpty(password);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
